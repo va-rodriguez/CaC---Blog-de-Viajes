451a69c18f7f680adf7a1f9f85fa83f6e6d459c1
Helga Van der Tuin <hjvdtuin@gmail.com>
2024-05-20 02:20:41 -0300

    Update ingreso.js

--- ingreso.js
+++ ingreso.js
@@ -1,21 +1,17 @@
-// Función para mostrar el formulario de login y ocultar el de registro
 function showLogin() {
     document.getElementById('login').style.display = 'block';
     document.getElementById('register').style.display = 'none';
 }
 
-// Función para mostrar el formulario de registro y ocultar el de login
 function showRegister() {
     document.getElementById('login').style.display = 'none';
     document.getElementById('register').style.display = 'block';
 }
 
-// Inicializa con el formulario de login visible
 document.addEventListener('DOMContentLoaded', (event) => {
     showLogin();
 });
 
-// Funcionalidad para el botón de login
 document.getElementById('loginBtn').addEventListener('click', (event) => {
     event.preventDefault();
     const username = document.getElementById('loginUsername').value;
@@ -31,7 +27,6 @@ document.getElementById('loginBtn').addEventListener('click', (event) => {
             icon: 'success',
             confirmButtonText: 'Continuar'
         }).then(() => {
-            // Redirigir a la página de inicio después de un login exitoso
             window.location.href = 'index.html';
         });
     } else {
@@ -44,16 +39,12 @@ document.getElementById('loginBtn').addEventListener('click', (event) => {
     }
 });
 
-// Funcionalidad para el botón de registro
 document.getElementById('registerBtn').addEventListener('click', (event) => {
     event.preventDefault();
     const username = document.getElementById('regUsername').value;
     const password = document.getElementById('regPassword').value;
 
-    // Lógica para registrar al nuevo usuario
     const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
-
-    // Verifica si el usuario ya existe
     const userExists = storedUsers.some(user => user.username === username);
 
     if (userExists) {
@@ -64,7 +55,6 @@ document.getElementById('registerBtn').addEventListener('click', (event) => {
             confirmButtonText: 'Intentar nuevamente'
         });
     } else {
-        // Registra el nuevo usuario
         storedUsers.push({ username: username, password: password });
         localStorage.setItem('users', JSON.stringify(storedUsers));
     Swal.fire({
@@ -73,8 +63,6 @@ document.getElementById('registerBtn').addEventListener('click', (event) => {
         icon: 'success',
         confirmButtonText: 'Continuar'
     });
-
-    // Después de registrar, puedes redirigir al usuario al login
     showLogin();
 }    
 });
